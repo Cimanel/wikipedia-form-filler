4761c0c15b144a26c419da7656e2a0f997f5b5db
JulienM <julienm@marmelab.com>
2024-04-24 16:51:51 +0200

    set container style

--- src/series/components/WikipediaAside.tsx
+++ src/series/components/WikipediaAside.tsx
@@ -1,4 +1,9 @@
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+
+import logo from "../../assets/wiki-ai-white.png";
 import {
   CONTENT_STATUS,
   LIST_STATUS,
@@ -15,11 +20,21 @@ export const WikipediaAside = () => {
   const { status } = useWikipediaContext();
 
   return (
-    <Box>
-      {status === SEARCH_STATUS && <WikipediaSearch />}
-      {status === LOADING_STATUS && <WikipediaLoading />}
-      {status === LIST_STATUS && <WikipediaList />}
-      {status === CONTENT_STATUS && <WikipediaContent />}
-    </Box>
+    <Card sx={{ width: 400, marginLeft: 2 }}>
+      <CardHeader
+        avatar={
+          <Box component="img" sx={{ width: 50, height: 50 }} src={logo} />
+        }
+        title="Wikipedia AI"
+        subheader="Fill form automatically"
+        sx={{ backgroundColor: "secondary.main" }}
+      />
+      <CardContent>
+        {status === SEARCH_STATUS && <WikipediaSearch />}
+        {status === LOADING_STATUS && <WikipediaLoading />}
+        {status === LIST_STATUS && <WikipediaList />}
+        {status === CONTENT_STATUS && <WikipediaContent />}
+      </CardContent>
+    </Card>
   );
 };
