4336d914112213966fbb746eaa63b2c415c7e2b9
Cimanel <cindy@marmelab.com>
2024-04-24 16:11:39 +0200

    update Wiki list

--- src/series/components/WikipediaList.tsx
+++ src/series/components/WikipediaList.tsx
@@ -1,8 +1,14 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { Button as RAButton, useDataProvider } from "react-admin";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useDataProvider } from "react-admin";
 import {
   LOADING_STATUS,
   SEARCH_STATUS,
@@ -13,6 +19,17 @@ export const WikipediaList = () => {
   const { setStatus, title, setWikipediaContent, wikipediaList } =
     useWikipediaContext();
   const dataProvider = useDataProvider();
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (value === "" && wikipediaList?.length > 0) {
+      setValue(wikipediaList[0].url);
+    }
+  }, [wikipediaList]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
 
   const handleCancel = () => {
     setStatus(SEARCH_STATUS);
@@ -21,6 +38,7 @@ export const WikipediaList = () => {
   const handleTitleClick = async (url: string) => {
     setStatus(LOADING_STATUS);
     const realTitle = url.split("/").pop();
+
     if (title) {
       const { data } = await dataProvider.getWikipediaContent({
         title: realTitle,
@@ -41,24 +59,37 @@ export const WikipediaList = () => {
         )}
         {wikipediaList?.length > 0 && (
           <>
-            <Box id="alert-dialog-title">
-              {"Select a source to fill the form:"}
-            </Box>
             <Stack>
-              {wikipediaList.map(({ title, url }) => {
-                return (
-                  <RAButton
-                    key={title}
-                    label={title}
-                    onClick={() => handleTitleClick(url)}
-                  />
-                );
-              })}
+              <FormControl>
+                <FormLabel id="demo-controlled-radio-buttons-group">
+                  {" "}
+                  Wikipedia articles matching the title &quot;{title}&quot;:
+                </FormLabel>
+                <RadioGroup
+                  name="use-radio-group"
+                  onChange={handleChange}
+                  value={value}
+                >
+                  {wikipediaList.map(({ title, url }) => {
+                    return (
+                      <FormControlLabel
+                        key={title}
+                        label={title}
+                        value={url}
+                        control={<Radio />}
+                      />
+                    );
+                  })}
+                </RadioGroup>
+              </FormControl>
             </Stack>
           </>
         )}
       </Box>
       <Box>
+        <Button onClick={() => handleTitleClick(value)} disabled={value === ""}>
+          Prefill with selected article
+        </Button>
         <Button onClick={handleCancel}>Cancel</Button>
       </Box>
     </Box>
